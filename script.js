a68abbf654fe9eeb31613386f0f32cc4264e2743
Muhammed Kasem <muhammedkasem2002@gmail.com>
2022-08-13 01:09:02 -0400

    Added resizing functionality to the grid

--- script.js
+++ script.js
@@ -4,35 +4,46 @@ const sliderValue = document.getElementById("sliderValue");
 const resetBtn = document.getElementById("resetBtn");
 sliderValue.innerHTML = slider.value + 'x' + slider.value;
 let gridSize = slider.value;
-let squares = gridSize * gridSize;
-let gridNumber = 0;
+let gridItems = document.querySelectorAll('.cell');
 createNewGrid();
 
 function createNewGrid() {
   gridSize = slider.value;
+  let squares = gridSize * gridSize;
   console.log(gridSize);
-} 
-
+  console.log(squares);
 for(i = 0; i < squares; i++) {
   grid.style.gridTemplateColumns = (`repeat(${gridSize}, 1fr`); 
   grid.style.gridTemplateRows = (`repeat(${gridSize}, 1fr`);
   let gridItem = document.createElement('div');
   gridItem.classList.add('cell');
   grid.insertAdjacentElement('beforeend', gridItem);
-  // gridItem.innerHTML = gridNumber += 1;
+  }
+  addListeners();
 }
 
-const gridItems = document.querySelectorAll('.cell');
-gridItems.forEach(cell => {
-  cell.addEventListener('mouseover', function handleClick(event) {
-    console.log('box clicked', event);
 
-    cell.setAttribute('style', 'background-color: salmon;');
+function addListeners() {
+gridItems = document.querySelectorAll('.cell');
+gridItems.forEach(cell => {
+    cell.addEventListener('mousedown', () => {
+    console.log('box clicked' );
+    cell.setAttribute('style', 'background-color: cyan;');
   });
 });
+}
 
-slider.oninput = function() {
+
+function removeChildren (grid) {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  console.log('removing Children!');
+}
+slider.onchange = function() {
+  removeChildren(grid);
   sliderValue.innerHTML = this.value + 'x' + this.value;
+  createNewGrid();
 }
 
 function resetGrid() {
