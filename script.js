3f2b23fbe8c2c8895720f8715b48bf7d64175e13
Muhammed Kasem <muhammedkasem2002@gmail.com>
2022-08-13 20:45:47 -0400

    Added color modes to grid and new user controls

--- script.js
+++ script.js
@@ -2,48 +2,68 @@ const grid = document.querySelector('.grid-container');
 const slider = document.getElementById("myRange");
 const sliderValue = document.getElementById("sliderValue");
 const resetBtn = document.getElementById("resetBtn");
+const colorPicker = document.getElementById("colorPicker");
+const rainbowBtn = document.getElementById('rainbowBtn')
 sliderValue.innerHTML = slider.value + 'x' + slider.value;
+
+
 let gridSize = slider.value;
 let gridItems = document.querySelectorAll('.cell');
+let currentColor = "default";
+
 createNewGrid();
 
 function createNewGrid() {
   gridSize = slider.value;
   let squares = gridSize * gridSize;
-  console.log(gridSize);
-  console.log(squares);
-for(i = 0; i < squares; i++) {
-  grid.style.gridTemplateColumns = (`repeat(${gridSize}, 1fr`); 
-  grid.style.gridTemplateRows = (`repeat(${gridSize}, 1fr`);
-  let gridItem = document.createElement('div');
-  gridItem.classList.add('cell');
-  grid.insertAdjacentElement('beforeend', gridItem);
+  for (i = 0; i < squares; i++) {
+    grid.style.gridTemplateColumns = (`repeat(${gridSize}, 1fr`);
+    grid.style.gridTemplateRows = (`repeat(${gridSize}, 1fr`);
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('cell');
+    gridItem.addEventListener('mouseover', setColors);
+    grid.insertAdjacentElement('beforeend', gridItem);
   }
-  addListeners();
 }
 
+function setColors(e) {
+  switch (currentColor) {
+    case "default":
+      e.target.style.backgroundColor = 'black';
+      break;
 
-function addListeners() {
-gridItems = document.querySelectorAll('.cell');
-gridItems.forEach(cell => {
-    cell.addEventListener('mousedown', () => {
-    console.log('box clicked' );
-    cell.setAttribute('style', 'background-color: cyan;');
-  });
-});
+    case "rainbow":
+      e.target.style.backgroundColor = `${randomRGB()}`;
+      break;
+
+    case "color":
+      e.target.style.backgroundColor = `${colorPicker.value}`;
+
+  }
 }
 
+function randomRGB() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  return "rgb(" + x + "," + y + "," + z + ")";
+}
 
-function removeChildren (grid) {
+function removeChildren(grid) {
   while (grid.firstChild) {
     grid.removeChild(grid.firstChild);
   }
-  console.log('removing Children!');
 }
+
 slider.onchange = function() {
   removeChildren(grid);
   sliderValue.innerHTML = this.value + 'x' + this.value;
   createNewGrid();
+}
+
+colorPicker.oninput = () => {
+  currentColor = "color";
+  setColors();
 }
 
 function resetGrid() {
@@ -53,3 +73,7 @@ function resetGrid() {
 }
 
 resetBtn.addEventListener('click', resetGrid);
+rainbowBtn.addEventListener('click', () => {
+  currentColor = "rainbow";
+  setColors();
+})
